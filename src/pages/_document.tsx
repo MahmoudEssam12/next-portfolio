a0e865c25ca2aa2b78e10d17cd5f9ba12cffa3d2
Mahmoud Essam <dev.mahmoud.essam@gmail.com>
2026-03-14 05:57:10 +0200

    Add Google Tag Manager integration to _document.tsx
    Incorporated GTM script for tracking and analytics, replacing the previous GTM iframe with a new implementation. This update enhances the site's tracking capabilities while ensuring compliance with best practices for script loading.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,21 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+      <Script 
+        id="gtm-script" 
+        strategy="beforeInteractive" 
+        src="https://www.googletagmanager.com/gtm.js?id=GTM-T7XHCMKH"
+        dangerouslySetInnerHTML={{ __html: `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-T7XHCMKH');
+        ` }} />
         <meta charSet="utf-8" />
         <meta name="theme-color" content="#0a0a0a" />
         <link rel="icon" href="/favicon.ico" />
@@ -43,16 +55,11 @@ export default function Document() {
         />
       </Head>
       <body>
+        <noscript><iframe 
+        src="https://www.googletagmanager.com/ns.html?id=GTM-T7XHCMKH"
+        height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
         <Main />
         <NextScript />
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TQ5B6443"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
       </body>
     </Html>
   );
